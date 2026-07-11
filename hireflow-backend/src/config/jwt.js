2eb19dbd58a10de1dec21d8f8161c9d9cd6fd825
pracheyyy <prachipatilm610@gmail.com>
2026-07-11 19:05:50 +0530

    deleted old version of auth

--- hireflow-backend/src/config/jwt.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  accessSecret: process.env.JWT_ACCESS_SECRET,
-  refreshSecret: process.env.JWT_REFRESH_SECRET,
-  accessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN || "15m",
-  refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || "7d",
-  // ms value used for the refresh cookie maxAge
-  refreshExpiresInMs: 7 * 24 * 60 * 60 * 1000,
-};
